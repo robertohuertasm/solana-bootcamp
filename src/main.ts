0070e2aa49a718cf3ec7a616b2b17f4541b7bd22
Roberto Huertas <roberto.huertas@outlook.com>
2024-02-06 23:27:09 +0100

    chore(setup): add initial dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { Buffer } from 'buffer';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(window as any).global = window;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(window as any).Buffer = Buffer;
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
