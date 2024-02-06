a11d60889a308d7b2e80f33d68775964b80871f3
Roberto Huertas <roberto.huertas@outlook.com>
2024-02-07 00:06:13 +0100

    adding process

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { Buffer } from 'buffer';
+import * as process from 'process';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 (window as any).global = window;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 (window as any).Buffer = Buffer;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(window as any).process = process;
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
